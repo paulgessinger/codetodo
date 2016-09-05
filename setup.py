74c5417b00dd70df0f1eda4493eb4d2c8dd685ce
Paul Gessinger <hello@paulgessinger.com>
2016-09-05 14:56:13 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # @TODO: Fix it! DONE
 setup(
     name="codetodo",
-    version="1.0",
+    version="1.1",
     install_requires=[
         "tabulate",
         "termcolor",
